c86ba60774485d21b6e0ec8562a4554330a79621
AxelReviron <105713060+AxelReviron@users.noreply.github.com>
2022-06-10 15:00:15 +0200

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<div class="conteneurHeader">
-    <img class='logo' src="IMG/logoGBAF.jpg" alt="Logo GBAF">
-    <p class="utilisateur">Nom & Prénom</p>
-    <div class="icon">
-        <a href="http://parametre.html"><img class='parametre' src="IMG/parametre.png" alt="parametre"></a>
-        <img class='deconnexion' src="IMG/deconnexion.png" alt="deconnexion">
-    </div> 
-</div>
-<hr>
