d1c86842eb0e8ffd0ab5baf109ce055d248ab2d3
Axel Reviron <axel.reviron@gmail.com>
2022-06-14 11:51:50 +0200

    Header & Footer des pages du site (pour utilisateur connecté)

---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="conteneurHeader">
+    <img class='logo' src="IMG/logoGBAF.jpg" alt="Logo GBAF">
+    <p class="utilisateur">
+        <?php if(isset($_SESSION['id_user'])) {
+            $connected_user = "$_SESSION[nom] $_SESSION[prenom]";
+            echo $connected_user;
+        } else {
+            $disconnected_user = "Vous n'êtes pas connecté";
+            echo $disconnected_user;
+        }?>
+    </p>
+    <div class="icon">
+        <a href="parametre.php"><img class='parametre' src="IMG/parametre.png" alt="parametre"></a>
+        <a href="deconnexion.php"><img class='deconnexion' src="IMG/deconnexion.png" alt="deconnexion"></a>
+    </div> 
+</div>
+<hr>
