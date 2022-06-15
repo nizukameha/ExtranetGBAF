77e7e932316415f2a4dba6031fef67a34d0f648b
Axel Reviron <axel.reviron@gmail.com>
2022-06-15 18:41:56 +0200

    Pour créer un nouveau mot de passe

--- newpassword.php
+++ newpassword.php
@@ -1,7 +1,44 @@
 <?php 
+include_once('DB/connexionDB.php');
+// Si l'utilisateur est connecté il est redirigé sur la page d'accueil
+if(!isset($_SESSION['id_user'])) {
+    header('Location: connexion.php');
+    exit;
+}
+// Requette a la bdd pour récupérer les données de l'utilisateur en fonction de son id
+$requette = $DB->prepare("SELECT *
+    FROM account 
+    WHERE id_user = ?");
+// récupere l'id_user de l'utilisateur
+$requette->execute([$_SESSION['id_user']]);
 
+$req = $requette->fetch();
+
+// Condition qui s'applique lorsque que les input du formulaire sont vides
+if(!empty($_POST)) {
+    extract($_POST);
+    $valid = (boolean) true;
+    // Condition qui s'applique lorsque l'utilisateur clique sur le boutton d'envoie du formulaire
+    if(isset($_POST['valider'])) {
+        // Le nom sera celui envoyé par l'utilisateur sans espace ni injection de code
+        $mdp = htmlspecialchars(trim($mdp));
+        // Si l'utilisateur ne rentre aucunes données la modification n'aura pas lieu
+        if(empty($mdp)) {
+            $valid = false;
+        }
+        // Si les modifications sont valides on met a jour la bdd et l'utilisateur est redirigé sur la page d'acceuil
+        if($valid) {
+            $crypt_mdp = password_hash($mdp, PASSWORD_BCRYPT);
+            $requette = $DB->prepare("UPDATE account SET mdp = ?
+            WHERE id_user = ?");
+            $requette->execute(array($crypt_mdp, $_SESSION['id_user']));
+
+            header('Location: connexion.php');
+            exit;
+        }
+    }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -18,7 +55,7 @@
         </header>
         <section class="conteneurImage">
             <img class='logoOpacity' src="IMG/logoGBAF.jpg" alt="Logo GBAF">   
-            <form action="newpassword.php">
+            <form action="newpassword.php" method="post">
                 <h1>Mot de passe oublié</h1><br>          
                 <label for="mdp">Nouveau mot de passe :</label><br>
                     <input type="password" name="mdp"><br>
