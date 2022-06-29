4acb3bc052635e8d7f32b196e45895db1d7b1d5f
Axel Reviron <axel.reviron@gmail.com>
2022-06-29 17:17:37 +0200

    Ajout d'un message et de l'email utilisateur dans la table contact

--- contact.php
+++ contact.php
@@ -1,6 +1,27 @@
 <?php 
 // Connexion a la base de données depuis le fichier 'connexionDB.php'
 include_once('DB/connexionDB.php');
+
+if(isset($_POST['valider'])) {
+// Si les champs "email" et "post" sont bien remplis
+    if(!empty($_POST['email']) && !empty($_POST['post'])) {
+        extract($_POST);
+        $post = htmlspecialchars($post);
+        $emailUtilisateur = htmlspecialchars($email);
+        // Verification que l'email existe bien
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $requette = $DB->prepare("INSERT INTO contact(email, post) VALUES (?, ?)");
+            $requette->execute(array($emailUtilisateur, $post));
+            $emailSend = "Votre demande de contact a été envoyé";
+        } else {
+            $badEmail = "Cet email n'existe pas !";
+        }
+
+    } else {
+            $err_com = "Votre commentaire est vide !";
+            $err_email = "Veuillez indiquer votre email !";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -16,13 +37,16 @@ include_once('DB/connexionDB.php');
         <header>
             <?php include('header.php'); ?>
         </header>
-        <form action="connexion.php" method="post">
+        <section class="conteneurImage">
+            <img class='logoOpacity' src="IMG/logoGBAF.jpg" alt="Logo GBAF">
+            <form action="contact.php" method="post">
                 <h1>Contact</h1><br>
-                <div class="erreur">
-                    <?php if(isset($err_mail)) { echo $err_mail; } ?>
-                </div> 
-                <label for="email">E-mail :</label><br>         
-                    <input type="text" name="email"><br>
+                <label for="email">E-mail :</label><br>
+                    <div class="erreur">
+                        <?php if(isset($badEmail)) { echo $badEmail; } ?>
+                        <?php if(isset($err_email)) { echo $err_email; } ?>
+                    </div> 
+                    <input type="email" name="email"><br>
                 <label for="post">Commentaire :</label><br>
                     <div class="erreur">
                         <?php if(isset($err_com)) { echo $err_com; } ?>
@@ -31,6 +55,8 @@ include_once('DB/connexionDB.php');
                 <div class="button">
                     <button type="submit" name="valider">Valider</button>
                 </div>
+                <?php if(isset($emailSend)) { echo $emailSend; } ?>
             </form>
+        </section>
     </body>
 </html>
