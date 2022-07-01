c20e58c42baeefb52b186c6a79252f56e280c756
Axel Reviron <axel.reviron@gmail.com>
2022-07-01 20:20:30 +0200

    Logo qui redirige sur la page d'accueil et suppression des boutons sur le coté droit du header

--- header.php
+++ header.php
@@ -1,18 +1,17 @@
 <div class="conteneurHeader">
-    <img class='logo' src="IMG/logoGBAF.jpg" alt="Logo GBAF">
-    <p class="utilisateur">
-        <?php if(isset($_SESSION['id_user'])) {
-            $connected_user = "$_SESSION[nom] $_SESSION[prenom]";
-            echo $connected_user;
-        } else {
-            $disconnected_user = "Vous n'êtes pas connecté";
-            echo $disconnected_user;
-        }?>
-    </p>
-    <div class="icon">
-        <a href="accueil.php"><img class='deconnexion' src="IMG/home.png" alt="deconnexion"></a>
-        <a href="parametre.php"><img class='parametre' src="IMG/parametre.png" alt="parametre"></a>
+    <a href="accueil.php"><img class='logo' src="IMG/logoGBAF.jpg" alt="Logo GBAF"></a>
+    <div class="user&icon">
+        <p class="utilisateur">
+            <a href="parametre.php"><img class='parametre' src="IMG/user.png" alt="parametre"></a>
+            <?php if(isset($_SESSION['id_user'])) {
+                $connected_user = "$_SESSION[nom] $_SESSION[prenom]";
+                echo $connected_user;
+            } else {
+                $disconnected_user = "Vous n'êtes pas connecté";
+                echo $disconnected_user;
+            }?>
+        </p>
         <a href="deconnexion.php"><img class='deconnexion' src="IMG/deconnexion.png" alt="deconnexion"></a>
-    </div> 
+    </div>
 </div>
 <hr>
