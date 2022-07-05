151de6ebcff3435cb47d4c9a8845bf3eb3edfce7
Axel Reviron <axel.reviron@gmail.com>
2022-07-05 12:03:12 +0200

    Possibilité de liké lorsqu'on a disliké et inversement

--- acteurs.php
+++ acteurs.php
@@ -27,11 +27,11 @@ if(!isset($_SESSION['id_user'])) {
             // Afficher les acteurs
             $id_acteur = $_GET['id'];
             // On récupere toutes les données de la table acteur en fonction de l'id
-            $requette = $DB->prepare("SELECT * 
+            $requete = $DB->prepare("SELECT * 
             FROM acteur 
             WHERE id_acteur = $id_acteur" );
-            $requette ->execute();
-            $acteurs = $requette->fetchAll();
+            $requete ->execute();
+            $acteurs = $requete->fetchAll();
             // On affiche les acteurs dans une boucle
             foreach ($acteurs as $acteur) {
             ?>
@@ -54,62 +54,80 @@ if(!isset($_SESSION['id_user'])) {
         <section class="conteneurActeurs">
             <div class="conteneurNewComment">
                 <?php
-                $requette = $DB->prepare("SELECT post FROM post WHERE id_acteur = ?");
-                $requette->execute(array($id_acteur));
-                $post = $requette->rowCount();
+                $requete = $DB->prepare("SELECT post FROM post WHERE id_acteur = ?");
+                $requete->execute(array($id_acteur));
+                $post = $requete->rowCount();
                 ?>
                 <span class="titreMobile>"><h2><?= $post; ?> commentaires</h2></span>
                 <div class="divNewComment">
                     <p><a href="#newcomment" class="newComment">Nouveau commentaire</a></p>
                 </div>
                 <?php
+                // Quand l'utilisateur clique sur "Like"
                 if(isset($_POST['like'])) {
-                    $requette = $DB->prepare("SELECT id_user, id_acteur FROM vote WHERE id_user = ? AND id_acteur = ?");
-                    $requette->execute(array($_SESSION['id_user'], $id_acteur));
-                    $requette = $requette->fetch();
-                    if($requette) {
-                    $errVote = "Vous avez déjà voté pour cet acteur !";
+                    $requete = $DB->prepare("SELECT * FROM vote WHERE id_user = ? AND id_acteur = ?");
+                    $requete->execute(array($_SESSION['id_user'], $id_acteur));
+                    $requete = $requete->fetch();
+                    // Si il y a un "Like" dans la bdd
+                    if($requete && $requete['vote'] == 1) {
+                        // L'utilisateur ne peut pas liké plusieurs fois
+                        $errVote = "Vous avez déjà voté pour cet acteur !";
+                    // Si il y a un "Dislike" dans la bdd alors on met a jour    
+                    } elseif($requete && $requete['vote'] == 0) {
+                        $id_user = $_SESSION['id_user'];
+                        $like = 1;
+                        $requete = $DB->prepare("UPDATE vote SET vote = ? WHERE id_user = ? AND id_acteur = ?");
+                        $requete ->execute(array($like, $_SESSION['id_user'], $id_acteur));
+                    // Si il n'y a ni "Like" ni "Dislike" dans la bdd alors on ajoute son "Like"
                     } else {
-                    extract($_POST);
-                    $id_user = $_SESSION['id_user'];
-                    $vote = 1;
-                    $requette = $DB->prepare("INSERT INTO vote(id_user, id_acteur, vote) 
-                    VALUES (?, ?, ?)");
-                    $requette ->execute(array($id_user, $id_acteur, $vote));
+                        extract($_POST);
+                        $id_user = $_SESSION['id_user'];
+                        $vote = 1;
+                        $requete = $DB->prepare("INSERT INTO vote(id_user, id_acteur, vote) 
+                        VALUES (?, ?, ?)");
+                        $requete ->execute(array($id_user, $id_acteur, $vote));
                     }
+                // Meme code ici quand l'utilisateur cliaue sur "Dislike"
                 } elseif(isset($_POST['dislike'])) {
-                    $requette = $DB->prepare("SELECT id_user, id_acteur FROM vote WHERE id_user = ? AND id_acteur = ?");
-                    $requette->execute(array($_SESSION['id_user'], $id_acteur));
-                    $requette = $requette->fetch();
-                    if($requette) {
-                    $errVote = "Vous avez déjà voté pour cet acteur !";
+                    $requete = $DB->prepare("SELECT * FROM vote WHERE id_user = ? AND id_acteur = ?");
+                    $requete->execute(array($_SESSION['id_user'], $id_acteur));
+                    $requete = $requete->fetch();
+                    if($requete && $requete['vote'] == 0) {
+                        $errVote = "Vous avez déjà voté pour cet acteur !";
+                    } elseif($requete && $requete['vote'] == 1) {
+                        $id_user = $_SESSION['id_user'];
+                        $dislike = 0;
+                        $requete = $DB->prepare("UPDATE vote SET vote = ? WHERE id_user = ? AND id_acteur = ?");
+                        $requete ->execute(array($dislike, $_SESSION['id_user'], $id_acteur));
                     } else {
-                    extract($_POST);
-                    $id_user = $_SESSION['id_user'];
-                    $vote = 0;
-                    $requette = $DB->prepare("INSERT INTO vote(id_user, id_acteur, vote) 
-                    VALUES (?, ?, ?)");
-                    $requette ->execute(array($id_user, $id_acteur, $vote));
+                        extract($_POST);
+                        $id_user = $_SESSION['id_user'];
+                        $vote = 0;
+                        $requete = $DB->prepare("INSERT INTO vote(id_user, id_acteur, vote) 
+                        VALUES (?, ?, ?)");
+                        $requete ->execute(array($id_user, $id_acteur, $vote));
                     }
                 }
                 ?>
                 <div class="conteneurLike">
                     <form action="acteurs.php?id=<?= $_GET['id'] ?>" method="post">
+                        <!-- On remplace le style du bouton "Envoyer" par une image -->
                         <button type="submit" class="likeButton" name="like"><img class="likeImage" src="IMG/like.png">
                             <?php
                                 $like = 1;
-                                $requette = $DB->prepare("SELECT vote FROM vote WHERE vote = ? AND id_acteur = ?");
-                                $requette->execute(array($like, $id_acteur));
-                                $votes = $requette->rowCount();
+                                // Afficher le nombre de "Like" pour chaque acteur
+                                $requete = $DB->prepare("SELECT vote FROM vote WHERE vote = ? AND id_acteur = ?");
+                                $requete->execute(array($like, $id_acteur));
+                                $votes = $requete->rowCount();
                                 echo $votes;
                             ?>
                         </button>
                         <button type="submit" class="dislikeButton" name="dislike"><img class="likeImage" src="IMG/dislike.png">
                             <?php
                                 $dislike = 0;
-                                $requette = $DB->prepare("SELECT vote FROM vote WHERE vote = ? AND id_acteur = ?");
-                                $requette->execute(array($dislike, $id_acteur));
-                                $votes = $requette->rowCount();
+                                $requete = $DB->prepare("SELECT vote FROM vote WHERE vote = ? AND id_acteur = ?");
+                                $requete->execute(array($dislike, $id_acteur));
+                                $votes = $requete->rowCount();
                                 echo $votes;
                             ?>
                         </button>
@@ -117,6 +135,7 @@ if(!isset($_SESSION['id_user'])) {
                 </div>
                 <br>
             </div>
+            <!-- Si l'utilisateur a déjà liké ou disliké on affiche un message d'erreur -->
             <div class="erreur">
                 <?php if (isset($errVote)) { echo $errVote; } ?>
             </div>
@@ -132,12 +151,12 @@ if(!isset($_SESSION['id_user'])) {
                         // Vérification que l'utilisateur n'a pas déjà commenté
                         } else {
                             // Selectionne l'id_user correspondant a l'id_user de la session pour cet acteur
-                            $requette = $DB->prepare("SELECT id_user, id_acteur FROM post WHERE id_user = ? AND id_acteur = ?");
-                            $requette->execute(array($_SESSION['id_user'], $id_acteur));
+                            $requete = $DB->prepare("SELECT id_user, id_acteur FROM post WHERE id_user = ? AND id_acteur = ?");
+                            $requete->execute(array($_SESSION['id_user'], $id_acteur));
                             // fetch et non pas fetchAll car on veut une valeur
-                            $requette = $requette->fetch();
-                            // Si la requette est vrai c'est que l'utilisateur a déjà commenté
-                            if($requette) {
+                            $requete = $requete->fetch();
+                            // Si la requete est vrai c'est que l'utilisateur a déjà commenté
+                            if($requete) {
                             $errUser = "Vous avez déjà commenté pour cet acteur !";
                             // Si il n'a jamais commenté, on ajoute son commentaire a la bdd
                             } else {
@@ -147,9 +166,9 @@ if(!isset($_SESSION['id_user'])) {
                                 $date_add = date("y.m.d");
                                 $post = htmlspecialchars($post);
                                 // Ajout a la base de donnée
-                                $requette = $DB->prepare("INSERT INTO post(id_user, id_acteur, date_add, post) 
+                                $requete = $DB->prepare("INSERT INTO post(id_user, id_acteur, date_add, post) 
                                 VALUES (?, ?, ?, ?)");
-                                $requette ->execute(array($id_user, $id_acteur, $date_add, $post));
+                                $requete ->execute(array($id_user, $id_acteur, $date_add, $post));
                             }
                         }
                     }
@@ -173,13 +192,13 @@ if(!isset($_SESSION['id_user'])) {
                     
                     <?php
                     // Afficher les commentaires lié aux utilisateurs et aux acteurs | INNER JOIN permet de lié plusieurs tables |
-                    $requette = $DB->prepare("SELECT post.*, prenom, id_acteur FROM post  
+                    $requete = $DB->prepare("SELECT post.*, prenom, id_acteur FROM post  
                     INNER JOIN account  
                     ON account.id_user = post.id_user
                     WHERE id_acteur = ?");
                     // On veux récupérer les commentaires des utilisateurs qui sont lié a cet acteur. Pour cela on précise l'id de l'acteur
-                    $requette->execute([$id_acteur]);
-                    $req_post = $requette->fetchAll();
+                    $requete->execute([$id_acteur]);
+                    $req_post = $requete->fetchAll();
                         foreach($req_post as $commentaire) {
                     ?>
                     <article class="acteur">
